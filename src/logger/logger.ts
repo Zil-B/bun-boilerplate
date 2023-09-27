b1811736e7e4351718bee74fe22bcd3ad7826167
zilayo <84344709+zilayo@users.noreply.github.com>
2023-09-27 10:38:47 +0100

    fix: winston stderrlevels

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -6,9 +6,17 @@ const myFormat = printf(({ level, message, timestamp }) => {
 });
 
 export const logger = createLogger({
-  level: "info",
-  transports: [new winston.transports.Console(), new winston.transports.File({ filename: "logs/log.log" })],
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ["error", "warn"],
+      level: "info",
+    }),
+    new winston.transports.File({ filename: "logs/log.log", level: "info" }),
+    new winston.transports.File({ filename: "logs/errors.log", level: "error" }),
+  ],
+
   format: combine(format.colorize(), format.timestamp({ format: "DD/MM/YYYY HH:mm:ss" }), myFormat),
   exceptionHandlers: [new transports.File({ filename: "logs/log-exceptions.log" })],
   rejectionHandlers: [new transports.File({ filename: "logs/log-exceptions.log" })],
+  exitOnError: false,
 });
